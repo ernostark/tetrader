fcc92798c3a2323daac33e2e2e8033a33e31d706
Stark Ernő <stark.erno@gmail.com>
2024-12-03 18:15:16 +0100

    Formok kész

---
+++ src/app/input/input.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css'
+})
+export class InputComponent {
+
+  startCalc(){
+    
+    console.log("Működik");
+    
+  }
+
+}
